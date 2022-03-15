a87221a3aaa83ff9ab034c03f843816db9a6100e
touati-ahlem <touatiiahlem@gmail.com>
2022-03-15 22:03:28 +0100

    init

---
+++ app.js
@@ -0,0 +1,16 @@
+const express =require('express');
+const app=express();
+
+app.get('/', (req, res) => {
+  res.send('test route => home page');
+});
+// Page Not founded
+app.use((req, res) => {
+  res.status(404).json({
+    msg: 'Page not founded',
+  });
+});
+
+app.listen(3000, () => {
+  console.log(`App listening on por !`);
+});
